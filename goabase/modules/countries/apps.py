eb10b47d557e6e638650fc0052954da51468636e
Eike Foken <e.foken@abian.de>
2016-10-07 15:37:02 +0200

    Configure translatable app verbose names

--- goabase/modules/countries/apps.py
+++ goabase/modules/countries/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class CountriesConfig(AppConfig):
     name = 'goabase.modules.countries'
+    verbose_name = _('Countries')
--- goabase/modules/parties/apps.py
+++ goabase/modules/parties/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class PartiesConfig(AppConfig):
     name = 'goabase.modules.parties'
+    verbose_name = _('Parties')
