35593aadfd76866c6f971a3d8153f8c9f133df12
Eike Foken <e.foken@abian.de>
2016-12-05 17:05:27 +0100

    SignupLoginModal

--- goabase/static/scripts/components/SignupLoginModal.js
+++ goabase/static/scripts/components/SignupLoginModal.js
@@ -1,76 +1,72 @@
 import qs from 'qs';
 
-const o = n(47)
-  , i = n(26)
-  , a = babelHelpers.interopRequireDefault(i)
-  , s = n(545)
-  , u = babelHelpers.interopRequireDefault(s)
-  , d = n(42)
-  , f = babelHelpers.interopRequireDefault(d)
-  , p = n(702)
-  , h = babelHelpers.interopRequireDefault(p)
-  , m = n(703)
-  , b = n(696)
-  , g = babelHelpers.interopRequireDefault(b)
-  , y = n(704)
-  , v = babelHelpers.interopRequireDefault(y)
-  , _ = n(710)
-  , k = babelHelpers.interopRequireDefault(_)
-  , T = n(712)
-  , w = babelHelpers.interopRequireDefault(T)
-  , E = n(11)
-  , S = babelHelpers.interopRequireDefault(E);
+import * as types from '../consts/ModalTypes';
+
+const o = {}; // n(47);
+// const s = n(545);
+const u = {}; // babelHelpers.interopRequireDefault(s);
+// const d = n(42);
+const f = {}; // babelHelpers.interopRequireDefault(d);
+// const b = n(696);
+const g = {}; // babelHelpers.interopRequireDefault(b);
+// const y = n(704);
+const v = {}; // babelHelpers.interopRequireDefault(y);
+// const _ = n(710);
+const k = {}; // babelHelpers.interopRequireDefault(_);
+// const T = n(712);
+const w = {}; // babelHelpers.interopRequireDefault(T);
+// const E = n(11);
+const S = {}; // babelHelpers.interopRequireDefault(E);
 
 class SignupLoginModal {
   launchSignup(e) {
-    function t() {
-      (0,
-      k.default)(),
-      o.callback && o.callback()
+    function callback() {
+      (0, k.default)();
+      if (o.callback) {
+        o.callback();
+      }
     }
-    function n() {
-      (0,
-      k.default)(),
-      o.onFinishedFlow && o.onFinishedFlow()
+
+    function onFinishedFlow() {
+      (0, k.default)();
+      if (o.onFinishedFlow) {
+        o.onFinishedFlow();
+      }
     }
-    function r() {
-      return (0,
-      k.default)(),
-      o.prioritizedCallbackReturningPromise ? o.prioritizedCallbackReturningPromise() : Promise.resolve()
+
+    function prioritizedCallbackReturningPromise() {
+      (0, k.default)();
+      return o.prioritizedCallbackReturningPromise ? o.prioritizedCallbackReturningPromise() : Promise.resolve();
     }
-    var o = e || {}
-      , i = {
-        path: window.location.pathname
+
+    const o = e || {};
+    const i = {
+      path: window.location.pathname,
     };
-    o.redirectUrl && (i.redirect_url = o.redirectUrl),
-    'auto' === o.launch_type && (i.launch_type = 'auto'),
-    o.ajax_endpoint && (i.ajax_endpoint = o.ajax_endpoint),
-    o.sticky === !0 && (i.sticky = 'true'),
-    o.business_email && (i.business_email = o.business_email,
-    (0,
-    S.default)().bt_sig && (i.business_email_sig = (0,
-    S.default)().bt_sig),
-    (0,
-    S.default)().bt_ts && (i.business_email_ts = (0,
-    S.default)().bt_ts)),
-    o.email && (i.email = o.email),
+
+    o.redirectUrl && (i.redirect_url = o.redirectUrl);
+    o.launch_type === 'auto' && (i.launch_type = 'auto');
+    o.ajax_endpoint && (i.ajax_endpoint = o.ajax_endpoint);
+    o.sticky === true && (i.sticky = 'true');
+    if (o.business_email) {
+      i.business_email = o.business_email;
+      (0, S.default)().bt_sig && (i.business_email_sig = (0, S.default)().bt_sig);
+      (0, S.default)().bt_ts && (i.business_email_ts = (0, S.default)().bt_ts);
+    }
+    o.email && (i.email = o.email);
     o.urlParams ? o.urlParams = String(o.urlParams) + '&' + String(qs.stringify(i)) : o.urlParams = qs.stringify(i);
     const a = this.getWindowLocationSearch();
     a.length > 0 && (o.urlParams = String(o.urlParams) + '&' + String(a.slice(1)));
-    const s = Object.assign({}, o, {
-      callback: t,
-      onFinishedFlow: n,
-      prioritizedCallbackReturningPromise: r,
-    });
-    this.setupSignupLogin(m.MODAL_TYPE_SIGNUP, s);
+    const s = Object.assign({}, o, { callback, onFinishedFlow, prioritizedCallbackReturningPromise });
+    this.setupSignupLogin(types.MODAL_TYPE_SIGNUP, s);
   }
 
   launchSignupOrLogin(e) {
-    h.default.bump('signup_login.launch_signup_or_login', 1, 'status:attempt'),
-    (0,
-    f.default)('hli') ? (h.default.bump('signup_login.launch_signup_or_login', 1, 'status:launch_login'),
-    this.launchLogin(e)) : (h.default.bump('signup_login.launch_signup_or_login', 1, 'status:launch_signup'),
-    this.launchSignup(e));
+    if ((0, f.default)('hli')) {
+      this.launchLogin(e);
+    } else {
+      this.launchSignup(e);
+    }
   }
 
   getWindowLocationSearch() {
@@ -78,98 +74,76 @@ class SignupLoginModal {
   }
 
   launchLogin(e) {
-    h.default.bump('signup_login.launch_login', 1, 'status:attempt'),
-    (0,
-    w.default)(),
-    this.setupSignupLogin(m.MODAL_TYPE_LOGIN, e);
+    (0, w.default)();
+    this.setupSignupLogin(types.MODAL_TYPE_LOGIN, e);
   }
 
   launchLogout(e) {
-    this.setupSignupLogin(m.MODAL_TYPE_LOGOUT, e);
+    this.setupSignupLogin(types.MODAL_TYPE_LOGOUT, e);
   }
 
   launchOtp() {
-    this.setupSignupLogin(m.MODAL_TYPE_OTP);
+    this.setupSignupLogin(types.MODAL_TYPE_OTP);
   }
 
   setupSignupLogin(e, t) {
-    var n = this
-      , r = t || {}
-      , i = Object.assign({}, r);
-    delete i.$container,
-    a.default.logEvent({
-      event_name: 'signup_login_flow',
-      event_data: Object.assign({
-        sub_event: e,
-        operation: 'impression',
-      }, i),
-    });
-    var s = r.callback
-      , u = r.urlParams
-      , l = r.flow
-      , c = this.getModalUrl(e, u)
-      , d = r.sticky;
-    this.setLoading(e, !0),
+    const n = this;
+    const r = t || {};
+    const i = Object.assign({}, r);
+    delete i.$container;
+    const s = r.callback;
+    const u = r.urlParams;
+    const l = r.flow;
+    const c = this.getModalUrl(e, u);
+    const d = r.sticky;
+    this.setLoading(e, true),
     this.closeModals();
-    var f = r.customType ? String(e) + '.' + String(r.customType) : e;
+    const f = r.customType ? String(e) + '.' + String(r.customType) : e;
     !r.$container && this.modals[f] ? (this.modals[f].open(),
     r.onModalOpen && r.onModalOpen(),
-    this.setLoading(e, !1),
-    g.default.page = e) : $.get(c, this.getRequestParams(r), function(t) {
-      var s = void 0;
-      if (r.$container ? r.$container.html(t.trim()) : (s = new o.Modal(t.trim(),{
-        sticky: d
-      }),
-      s.open(),
-      r.onModalOpen && r.onModalOpen(),
-      s.on('close', function() {
-        v.default.handleModalClose(n.modalIsLoading),
-        a.default.logEvent({
-          event_name: 'signup_login_flow',
-          event_data: {
-            sub_event: 'nav_click_away',
-            operation: 'impression',
-            modal: e,
-          },
-        })
-      }),
-      n.modals[f] = s),
-      [m.MODAL_TYPE_SIGNUP, m.MODAL_TYPE_LOGIN].indexOf(e) !== -1) {
-        var u = $('#link_alipay_user_id')
-          , l = !!u.val();
+    this.setLoading(e, false),
+    g.default.page = e) : $.get(c, this.getRequestParams(r), (t) => {
+      let s = void 0;
+
+      if (r.$container) {
+        r.$container.html(t.trim());
+      } else {
+        s = new o.Modal(t.trim(), {
+          sticky: d,
+        });
+        s.open();
+        r.onModalOpen && r.onModalOpen();
+        s.on('close', () => {
+          v.default.handleModalClose(n.modalIsLoading);
+        });
+        n.modals[f] = s;
+      }
+
+      if ([types.MODAL_TYPE_SIGNUP, types.MODAL_TYPE_LOGIN].indexOf(e) !== -1) {
+        const u = $('#link_alipay_user_id');
+        const l = !!u.val();
         g.default.init({
           page: e,
-          inModal: !0,
+          inModal: true,
           onFinishedFlow: r.onFinishedFlow,
           prioritizedCallbackReturningPromise: r.prioritizedCallbackReturningPromise,
           shouldInitPhoneNumberSignup: l,
-        })
+        });
       }
-      var p = r.$container || s.$element;
-      o.Tooltip && o.Tooltip.bind(p),
-      n.setLoading(e, !1),
-      'auto' === r.launch_type ? (v.default.addModalBackground(),
-      a.default.logEvent({
-        event_name: 'signup_login_flow',
-        event_data: {
-          sub_event: 'signup_launch_auto',
-          operation: 'impression',
-          modal_url: c,
-        },
-      }),
-      h.default.bump('signup_login.setup_signup_login', 1, 'launch_type:auto')) : (a.default.logEvent({
-        event_name: 'signup_login_flow',
-        event_data: {
-          sub_event: 'signup_launch_else',
-          operation: 'impression',
-          modal_url: c,
-        },
-      }),
-      h.default.bump('signup_login.setup_signup_login', 1, 'launch_type:else')),
-      p.find('input[placeholder], textarea[placeholder]').placeholder(),
-      p.on('click', '.modal-link', n.handleModalChange.bind(n, i)),
-      e === m.MODAL_TYPE_LOGOUT && n.listenForLogin(),
-      n.modals[f] = s
+      const p = r.$container || s.$element;
+      o.Tooltip && o.Tooltip.bind(p);
+      n.setLoading(e, false);
+
+      if ('auto' === r.launch_type) {
+        v.default.addModalBackground();
+      }
+
+      p.find('input[placeholder], textarea[placeholder]').placeholder();
+      p.on('click', '.modal-link', n.handleModalChange.bind(n, i));
+      if (e === types.MODAL_TYPE_LOGOUT) {
+        n.listenForLogin();
+      }
+      n.modals[f] = s;
     }),
     s && g.default.addLoginCallback(s),
     l && g.default.addFlow(l);
@@ -185,12 +159,13 @@ class SignupLoginModal {
     const type = ev.target.getAttribute('data-modal-type');
 
     switch (type) {
-      case m.MODAL_TYPE_LOGIN:
+      case types.MODAL_TYPE_LOGIN:
         this.launchLogin(e);
         break;
-      case m.MODAL_TYPE_SIGNUP:
+      case types.MODAL_TYPE_SIGNUP:
         this.launchSignup(e);
         break;
+      // no default
     }
   }
 
@@ -201,32 +176,37 @@ class SignupLoginModal {
   }
 
   setLoading(e, t) {
-    const n = $('.airbnb-header [data-' + String(e) + '-modal]');
-    const r = 'link-disabled';
-    t ? (this.modalIsLoading = !0,
-    n.addClass(r),
-    n.attr('disabled', !0)) : (v.default.removeSignupGardenBanner(),
-    this.modalIsLoading = !1,
-    n.removeClass(r),
-    n.removeAttr('disabled'));
+    const $modal = $(`.airbnb-header [data-${e}-modal]`);
+
+    if (t) {
+      this.modalIsLoading = true;
+      $modal.addClass('link-disabled');
+      $modal.attr('disabled', true);
+    } else {
+      v.default.removeSignupGardenBanner();
+      this.modalIsLoading = false;
+      $modal.removeClass('link-disabled');
+      $modal.removeAttr('disabled');
+    }
   }
 
   getModalUrl(type, params) {
     let url;
 
     switch (type) {
-      case m.MODAL_TYPE_LOGIN:
+      case types.MODAL_TYPE_LOGIN:
         url = '/login_modal';
         break;
-      case m.MODAL_TYPE_SIGNUP:
+      case types.MODAL_TYPE_SIGNUP:
         url = '/signup_modal';
         break;
-      case m.MODAL_TYPE_LOGOUT:
+      case types.MODAL_TYPE_LOGOUT:
         url = '/signed_out_modal';
         break;
-      case m.MODAL_TYPE_OTP:
+      case types.MODAL_TYPE_OTP:
         url = '/otp_modal';
         break;
+      // no default
     }
 
     if (params) {
@@ -238,7 +218,7 @@ class SignupLoginModal {
   init() {
     this.initEvents();
     this.initClickEvents();
-    setTimeout(function() {
+    setTimeout(() => {
       v.default.initSignupPrompt();
     }, 0);
   }
@@ -252,56 +232,32 @@ class SignupLoginModal {
 
   initClickEvents() {
     if (!['/login', '/signup_login'].includes(window.location.pathname)) {
-      $(document).on('click', '[data-login-modal]', function(ev) {
+      $(document).on('click', '[data-login-modal]', (ev) => {
         ev.preventDefault();
         const t = $(ev.currentTarget);
-        h.default.bump('signup_login.click_login_modal', 1),
-        t.parents('#header').length ? a.default.logEvent({
-          event_name: 'signup_login_flow',
-          event_data: {
-            sub_event: 'login_launch_nav',
-            operation: 'impression',
-          },
-        }) : 'banner-login-button' === t.attr('id') && a.default.logEvent({
-          event_name: 'signup_login_flow',
-          event_data: {
-            sub_event: 'login_launch_banner',
-            operation: 'impression',
-          },
-        }),
-        u.default.emit('login-modal:open')
+        if (!t.parents('#header').length) {
+          u.default.emit('login-modal:open');
+        }
       });
-      $(document).on('click', '[data-signup-modal]', function(ev) {
-        ev.preventDefault(),
-        h.default.bump('signup_login.click_signup_modal', 1);
+      $(document).on('click', '[data-signup-modal]', (ev) => {
+        ev.preventDefault();
         const t = $(ev.currentTarget);
-        t.parents('#header').length ? a.default.logEvent({
-          event_name: 'signup_login_flow',
-          event_data: {
-            sub_event: 'signup_launch_nav',
-            operation: 'impression',
-          },
-        }) : 'banner-signup-button' === t.attr('id') && a.default.logEvent({
-          event_name: 'signup_login_flow',
-          event_data: {
-            sub_event: 'signup_launch_banner',
-            operation: 'impression',
-          },
-        }),
-        u.default.emit('signup-modal:open')
+        if (!t.parents('#header').length) {
+          u.default.emit('signup-modal:open');
+        }
       });
     }
   }
 
   listenForLogin() {
-    u.default.once('login', function(e) {
-        e && e.refresh === !1 || window.location.reload()
+    u.default.once('login', (e) => {
+      e && e.refresh === false || window.location.reload();
     });
   }
 
   getRequestParams() {
-    var e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : {}
-      , t = {};
+    const e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : {};
+    const t = {};
     return e.$container && (t.embed = 1),
     e.redirectUrl && (t.redirect_url = e.redirectUrl),
     t;
---
+++ goabase/static/scripts/consts/ModalTypes.js
@@ -0,0 +1,4 @@
+export const MODAL_TYPE_LOGIN = 'login';
+export const MODAL_TYPE_LOGOUT = 'logout';
+export const MODAL_TYPE_OTP = 'otp';
+export const MODAL_TYPE_SIGNUP = 'signup';
